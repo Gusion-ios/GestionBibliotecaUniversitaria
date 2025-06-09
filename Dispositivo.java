1ea8b91cee3d91f5433167fe06b850e30fad5f11
jkc19 <106788242+jkc19@users.noreply.github.com>
2025-06-09 09:32:08 -0500

    Create Dispositivo.java

---
+++ Dispositivo.java
@@ -0,0 +1,25 @@
+public class Dispositivo {
+    private String tipo;
+    private String codigo;
+
+    public Dispositivo(String tipo, String codigo) {
+        this.tipo = tipo;
+        this.codigo = codigo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
