836f6b6e4a89320511809add18f4b85d2a841d1c
Benhazcode <benprosumina@gmail.com>
2025-07-13 20:50:42 -0500

    Update LibroDigitalMultimedia.java

--- src/LibroDigitalMultimedia.java
+++ src/LibroDigitalMultimedia.java
@@ -1,38 +1,73 @@
 public class LibroDigital extends Multimedia {
-    private String formato;
+
     private double tamaño;
 
-    public LibroDigital(int codigo, String titulo, String autor, boolean disponible,
-                        String formato, double tamaño) {
-        super(codigo, titulo, autor, disponible);
-        this.formato = formato;
+    private String formato;
+
+    public LibroDigital(int codigo, String titulo, String autor, boolean disponible, double tamaño, String formato, CategoriaMultimedia categoriaMultimedia) {
+
+        super(codigo, titulo, autor, disponible, categoriaMultimedia);
+
         this.tamaño = tamaño;
-    }
 
-    public String getFormato() {
-        return formato;
-    }
-
-    public void setFormato(String formato) {
         this.formato = formato;
+
+        this.categoriaMultimedia = categoriaMultimedia;
+
     }
 
     public double getTamaño() {
+
         return tamaño;
     }
 
     public void setTamaño(double tamaño) {
+
         this.tamaño = tamaño;
     }
 
-    @Override
-    public void mostrarInfo() {
-        System.out.println("Libro Digital:");
-        System.out.println("Código: " + codigo);
-        System.out.println("Título: " + titulo);
-        System.out.println("Autor: " + autor);
-        System.out.println("Formato: " + formato);
-        System.out.println("Tamaño: " + tamaño + " MB");
-        System.out.println("Disponible: " + disponible);
+    public String getFormato() {
+
+        return formato;
     }
+
+    public void setFormato(String formato) {
+
+        this.formato = formato;
+    }
+
+    public CategoriaMultimedia getCategoriaMultimedia() {
+
+        return categoriaMultimedia;
+    }
+
+    public void setCategoriaMultimedia(CategoriaMultimedia categoriaMultimedia) {
+
+        this.categoriaMultimedia = categoriaMultimedia;
+    }
+
+    @Override
+
+    public void mostrarInfo() {
+
+        System.out.println("Codigo: "+ codigo);
+
+        System.out.println("Titulo: "+ titulo);
+
+        System.out.println("Autor: "+ autor);
+
+        System.out.println("Tamaño: "+ tamaño);
+
+        System.out.println("Formato: "+ formato);
+
+        System.out.println("Disponible: "+ disponible);
+
+        System.out.println("Categoría: " + categoriaMultimedia.getCategoria());
+
+        System.out.println("Descripción: " + categoriaMultimedia.getDescripcion());
+
+
+    }
+
 }
+
